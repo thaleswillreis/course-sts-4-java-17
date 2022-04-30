c3182f49f67120f327f9d87376e50e1be431def1
thaleswillreis <thaleswill@yahoo.com.br>
2022-04-30 11:14:18 -0300

    Subtotal & Total methods

--- src/main/java/com/homeitz/course/entities/OrderItem.java
+++ src/main/java/com/homeitz/course/entities/OrderItem.java
@@ -66,6 +66,10 @@ public class OrderItem  implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() { 		//subtotal do pedido
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
